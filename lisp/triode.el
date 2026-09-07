e6453e6f7763be0e85888904a12c08df12956492
Charles Choi <kickingvegas@gmail.com>
2026-09-07 12:19:08 -0700

    Bump version to 0.0.1

--- lisp/triode.el
+++ lisp/triode.el
@@ -5,7 +5,7 @@
 ;; Author: Charles Y. Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/triode
 ;; Keywords: tools
-;; Package-Version: 0.0.1-rc.1
+;; Package-Version: 0.0.1
 ;; Package-Requires: ((emacs "30.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
